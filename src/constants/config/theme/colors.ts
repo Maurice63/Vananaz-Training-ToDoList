2c319bd0046d233fd053bc3deca9b81d3a645ba0
john <121928252+Maurice63@users.noreply.github.com>
2023-01-06 11:37:09 +0800

    added atomic design and colors constants

---
+++ src/constants/config/theme/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+    black: "#000000",
+    white: "FFFFFF",
+    //blue
+    bluePrimary: "#2F80ED",
+    blueSecondary: "#9ABFF1",
+    //lightblue
+    lightBluePrimary: "#AECDF7",
+    //red
+    redPrimary: "#ED2F2F",
+    //pink
+    pinkPrimary: "#E78585",
+    pinkSecondary: "#FCE0E0",
+    //green
+    greenPrimary: "#6FCF97"
+    }
+    export default colors;
