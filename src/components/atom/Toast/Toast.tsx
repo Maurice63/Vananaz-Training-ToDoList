e77ee7c2d874a46e4424edb3ae0520cba3c3dce8
john <121928252+Maurice63@users.noreply.github.com>
2023-01-10 17:02:36 +0800

    adding suggestions to files

--- src/components/atom/Toast/Toast.tsx
+++ src/components/atom/Toast/Toast.tsx
@@ -2,8 +2,7 @@ import { message as AntToast } from 'antd';
 import { MessageContainer,GlobalStyle } from './elements';
 import { ToastProps } from './types';
 
-const Toast = (props: ToastProps) => {
-    const {style,duration,message} = props
+const Toast = ({style,duration,message}: ToastProps) => {
     return(<>
         {AntToast.config({
         transitionName: 'move-down',
@@ -11,10 +10,11 @@ const Toast = (props: ToastProps) => {
         {
         AntToast.success({
             content: <MessageContainer>{message}</MessageContainer>,
-            duration: duration,
+            duration: duration || 3,
             icon: " ",
             className: 'custom-class',
-            style: {...style,
+            style: {
+              ...style,
               color:"black",
               padding: "0px",
             },
